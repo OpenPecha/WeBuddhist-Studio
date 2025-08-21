365c06672f0451cad4bd24d8e1c962aaadf0ce4b
TenzDelek <tibetdelek@gmail.com>
2025-08-21 22:39:25 +0530

    ui improvement

--- src/components/ui/molecules/dashboard-table/DashBoardTable.tsx
+++ src/components/ui/molecules/dashboard-table/DashBoardTable.tsx
@@ -1,13 +1,14 @@
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "../../atoms/table"
 import { Input } from "../../atoms/input"
 import { Button } from "../../atoms/button"
+import { Search } from "lucide-react"
 
 const plans = [
   {
     id: "1",
     coverImage: "https://ep-space.nyc3.cdn.digitaloceanspaces.com/app/uploads/2021/03/21161102/4-5dllc1155insidetengboche7-5x5cpywrt-2.jpg",
     title: "The 7 day buddhist Plan on Compassion",
-    subtitle: "this plan is for the people to learn and study about compassion in a boarder context of how we perceive it. the greatest way to do this is to understand .....",
+    subtitle: "this plan is for the people to learn and study about compassion in a boarder context of how we perceive it. the greatest way to do this is to understand",
     planDay: "7 Days",
     planUsed: "30 Used",
   },
@@ -15,7 +16,7 @@ const plans = [
     id: "2",
     coverImage: "https://static01.nyt.com/images/2023/09/28/multimedia/00mongolia-lama-explainer-kljz/00mongolia-lama-explainer-kljz-mediumSquareAt3X.jpg",
     title: "The 7 day buddhist Plan on Meditation",
-    subtitle: "this plan is for the people to learn and study about compassion in a boarder context of how we perceive it. the greatest way to do this is to understand .....",
+    subtitle: "this plan is for the people to learn and study about compassion in a boarder context of how we perceive it. the greatest way to do this is to understand ",
     planDay: "7 Days",
     planUsed: "3 Used",
   },
@@ -23,7 +24,7 @@ const plans = [
     id: "3",
     coverImage: "https://savetibet.org/wp-content/uploads/2019/06/shutterstock_1124150498-1000.jpg",
     title: "The 12 day buddhist Plan on Studying",
-    subtitle: "this plan is for the people to learn and study about compassion in a boarder context of how we perceive it. the greatest way to do this is to understand .....",
+    subtitle: "this plan is for the people to learn and study about compassion in a boarder context of how we perceive it. the greatest way to do this is to understand ",
     planDay: "12 Days",
     planUsed: "10 Used",
   },
@@ -41,10 +42,11 @@ export function DashBoardTable() {
 
   return (
     <div className="w-full">
-      <div className="mb-4">
+      <div className="mb-4 border w-fit  px-2 rounded-md border-gray-200 dark:border-[#313132] flex items-center ">
+        <Search className="w-4 h-4" />
         <Input
-          placeholder="Search Plan......"
-          className="w-1/3 rounded-md border px-4 py-2"
+          placeholder="Search Plan..."
+          className="rounded-md border-none dark:bg-transparent px-4 shadow-none py-2"
         />
       </div>
       <Table>
