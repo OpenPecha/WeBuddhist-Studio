bb8a8d7c07fbb24990abf94156cd045b3dd4b544
TenzDelek <tibetdelek@gmail.com>
2025-09-02 12:38:26 +0530

    encrpytion

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,17 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import sha256 from "crypto-js/sha256";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function createPasswordHash(
+  email: string,
+  password: string,
+  salt?: string,
+): string {
+  const envSalt = salt || import.meta.env.VITE_ENV_SALT || "";
+  const combinedString = email + envSalt + password;
+  return sha256(combinedString).toString();
 }
