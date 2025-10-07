5cb7a9c01a88faff9637af512b645e966b939501
TenzDelek <tibetdelek@gmail.com>
2025-10-07 10:05:11 +0530

    call proper api

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -3,6 +3,8 @@ export const ACCESS_TOKEN = "accessToken";
 export const RESET_PASSWORD = "resetPassword";
 export const RESET_PASSWORD_TOKEN = "resetPasswordToken";
 export const REFRESH_TOKEN = "refreshToken";
+export const NO_PROFILE_IMAGE =
+  "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png";
 export const BACKEND_BASE_URL =
   import.meta.env.VITE_BACKEND_BASE_URL || "https://api.webuddhist.com";
 export const LANGUAGE = "language";
