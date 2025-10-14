3609c1047f493bd5621ff92805ca979b467c5d80
TenzDelek <tibetdelek@gmail.com>
2025-10-14 11:33:23 +0530

    change

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -6,7 +6,7 @@ export const REFRESH_TOKEN = "refreshToken";
 export const NO_PROFILE_IMAGE =
   "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png";
 export const BACKEND_BASE_URL =
-  import.meta.env.VITE_BACKEND_DEV_BASE_URL || "http://localhost:3000";
+  import.meta.env.VITE_BACKEND_BASE_URL || "http://localhost:3000";
 export const LANGUAGE = "language";
 export const DIFFICULTY = [
   { label: "Beginner", value: "BEGINNER" },
