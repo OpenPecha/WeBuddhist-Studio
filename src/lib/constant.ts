2e2f2cc76bab78835e6b8c1c4dd7b740f6b7265a
TenzDelek <tibetdelek@gmail.com>
2025-10-14 11:11:49 +0530

    FIX TRY

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -5,8 +5,7 @@ export const RESET_PASSWORD_TOKEN = "resetPasswordToken";
 export const REFRESH_TOKEN = "refreshToken";
 export const NO_PROFILE_IMAGE =
   "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png";
-export const BACKEND_BASE_URL =
-  import.meta.env.VITE_BACKEND_BASE_URL || "https://api.webuddhist.com";
+export const BACKEND_BASE_URL = import.meta.env.VITE_BACKEND_DEV_BASE_URL;
 export const LANGUAGE = "language";
 export const DIFFICULTY = [
   { label: "Beginner", value: "BEGINNER" },
