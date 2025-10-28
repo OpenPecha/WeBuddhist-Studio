834e2b3306dd3ed96b458966f84f2bec01994a73
TenzDelek <tibetdelek@gmail.com>
2025-10-28 12:26:35 +0530

    changes

--- src/components/routes/task/api/taskApi.ts
+++ src/components/routes/task/api/taskApi.ts
@@ -110,7 +110,7 @@ export const updateTaskTitle = async (task_id: string, title: string) => {
 
 export const ChangeTaskDay = async (task_id: string, target_day_id: string) => {
   const accessToken = sessionStorage.getItem("accessToken");
-  const { data } = await axiosInstance.put(
+  const { data } = await axiosInstance.patch(
     `/api/v1/cms/tasks/${task_id}/day`,
     {
       target_day_id,
